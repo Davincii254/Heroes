2cad23726d183e21c91c050dd950d3e8b749e238
Davincii254 <victorkuria7@gmail.com>
2022-05-05 10:57:10 +0300

    Initial code

---
+++ src/main/java/App.java
@@ -0,0 +1,15 @@
+
+import org.sql2o.Sql2o;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.*;
+
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+}
+}
