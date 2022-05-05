9fec0b3e9742ee6b2351f2c01b14d3162b3fd927
Davincii254 <victorkuria7@gmail.com>
2022-05-05 13:58:54 +0300

    Update hbd files and add sql2oSquadDa

--- src/main/java/dao/sql2oSquadDao.java
+++ src/main/java/dao/sql2oSquadDao.java
@@ -2,7 +2,9 @@ package dao;
 
 import models.Hero;
 import models.Squad;
+import org.sql2o.Connection;
 import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
 
 import java.util.List;
 
@@ -22,33 +24,76 @@ public class sql2oSquadDao implements SquadDao{
         }
     }
 
+
     @Override
     public List<Squad> getAllSquads() {
-        return null;
+        getDrivers();
+        String sql = "SELECT * FROM squads";
+        try(Connection conn = sql2o.open()){
+            return conn.createQuery(sql)
+                    .executeAndFetch(Squad.class);
+        }
     }
 
     @Override
     public void addSquad(Squad squad) {
-
+        getDrivers();
+        String sql = "INSERT INTO squads (squadName,squadCause,maxSize) VALUES (:squadName, :squadCause, :maxSize)";
+        try(Connection conn = sql2o.open()){
+            int id = (int) conn.createQuery(sql,true)
+                    .bind(squad)
+                    .executeUpdate()
+                    .getKey();
+            squad.setId(id);
+        }catch(Sql2oException e){
+            System.out.println(e);
+        }
     }
 
     @Override
     public Squad findSquadById(int id) {
-        return null;
+        getDrivers();
+        String sql = "SELECT * FROM squads WHERE  id = :id";
+        try(Connection conn = sql2o.open()){
+            return conn.createQuery(sql)
+                    .addParameter("id",id)
+                    .executeAndFetchFirst(Squad.class);
+        }
     }
 
     @Override
     public List<Hero> getAllHeroesBySquad(int squadId) {
-        return null;
+        getDrivers();
+        try(Connection conn = sql2o.open()){
+            return conn.createQuery("SELECT * FROM heros WHERE squadId = :squadId")
+                    .addParameter("squadId",squadId)
+                    .executeAndFetch(Hero.class);
+        }
     }
 
     @Override
     public void deleteSquadById(int id) {
-
+        getDrivers();
+        String sql = "DELETE FROM squads WHERE id = :id";
+        try(Connection conn = sql2o.open()){
+            conn.createQuery(sql)
+                    .addParameter("id",id)
+                    .executeUpdate();
+        }catch(Sql2oException e){
+            System.out.println(e);
+        }
     }
 
     @Override
     public void deleteAllSquads() {
-
+        getDrivers();
+        String sql = "DELETE FROM squads";
+        try(Connection conn = sql2o.open()){
+            conn.createQuery(sql)
+                    .executeUpdate();
+        }catch(Sql2oException e){
+            System.out.println(e);
+        }
     }
 }
+
